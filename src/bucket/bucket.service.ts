275047e9a6a27c53e3c0e82e608071ff8a1a5058
gabriel-victor933 <gabrielvictor933@usp.br>
2024-10-03 16:50:27 -0300

    fix: changing variables name

--- src/bucket/bucket.service.ts
+++ src/bucket/bucket.service.ts
@@ -16,8 +16,8 @@ export class BucketService {
     this.s3Client = new S3Client({
       region: process.env.AWS_BUCKET_REGION,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_BUCKET_SECRET_ACCESS_KEY,
       },
     });
   }
